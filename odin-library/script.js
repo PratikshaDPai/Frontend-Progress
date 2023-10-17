240d237c8dd658ef8654166e3737410e8f49a2fe
ppai1312 <1312pratiksha@gmail.com>
2023-10-17 02:36:07 -0700

    Add styles.css,take in user input via forms, basic frontend

--- odin-library/script.js
+++ odin-library/script.js
@@ -3,30 +3,57 @@
 // Put a function into the constructor that can report the book info like so:
 // theHobbit.info() // "The Hobbit by J.R.R. Tolkien, 295 pages, not read yet"
 
-function Book(title, author, pages, readBook) {
+function Book(title, author, pages) {
   this.title = title;
   this.author = author;
   this.pages = pages;
-  this.readBook = readBook;
-  this.info = () => `${title} by ${author},${pages} pages,${readBook}`;
+  this.info = () => `${title} by ${author},${pages} pages`;
 }
 
 function addBookToLibrary() {
-  const title = prompt("Enter book title");
+  const title = document.querySelector("#title").value;
   let name = title;
-  const author = prompt("Enter book author");
-  const pages = prompt("Enter number of pagess in book");
-  const readBook = prompt(
-    "Enter if you have read the book(read yet/not read yet)"
-  );
-  name = new Book(title, author, pages, readBook);
-  return name.info();
+  const author = document.querySelector("#author").value;
+  const pages = document.querySelector("#pages").value;
+  name = new Book(title, author, pages);
+  return [name.title, name.info()];
+}
+
+function displayBookInfo(bookID, myLibrary) {
+  const books = document.querySelectorAll(".new-book-icon");
+  for (const book of books) {
+    book.addEventListener("click", () => {
+      const key = bookID.indexOf(book.id);
+      const info = myLibrary[key][1];
+      alert(info);
+    });
+  }
 }
 
 const myLibrary = [];
-const books = prompt("How many books do you have in your library?");
-// eslint-disable-next-line no-plusplus
-for (let book = 0; book < books; book++) {
-  myLibrary.push(addBookToLibrary());
-}
-console.log(myLibrary);
+const bookID = [];
+const newBookForm = document.querySelector("#new-book-form");
+const container = document.querySelector(".container");
+let newBookIcon;
+const newBook = document.querySelector("#new-book");
+newBook.addEventListener("click", () => {
+  newBookForm.style.display = "block";
+});
+
+const submit = document.querySelector("#submit");
+submit.addEventListener("click", () => {
+  const addNewBook = addBookToLibrary();
+  const newBookID = addNewBook[0];
+  bookID.push(addNewBook[0]);
+  myLibrary.push(addNewBook);
+  const bookTitle = document.createElement("p");
+  bookTitle.classList.add("book-title");
+  bookTitle.innerHTML = newBookID;
+  newBookIcon = document.createElement("div");
+  newBookIcon.classList.add("new-book-icon");
+  newBookIcon.setAttribute("id", newBookID);
+  container.appendChild(newBookIcon);
+  newBookIcon.appendChild(bookTitle);
+  displayBookInfo(bookID, myLibrary);
+  newBookForm.style.display = "none";
+});
